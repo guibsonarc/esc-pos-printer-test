6cfabbfe83fa35a44b7013b6acd933c1de3dd376
Guibson Arcebispo <guibsonarc@gmail.com>
2020-10-16 10:54:43 -0300

    Initial commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:oktoast/oktoast.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OKToast(
+      child: MaterialApp(
+        navigatorKey: Modular.navigatorKey,
+        title: 'ESC/POS Test',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        onGenerateRoute: Modular.generateRoute,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:esc_pos_printer_test/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
